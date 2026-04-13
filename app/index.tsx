ae66c3cd7b638b90390de3ad133b8462aec73754
Grishchenko, Ivan <bben.rasha@gmail.com>
2026-04-13 19:48:46 +0300

    chore(): setup project for running on phones

--- app/index.tsx
+++ app/index.tsx
@@ -2,35 +2,20 @@ import { Button } from '@components/ui/button';
 import { Icon } from '@components/ui/icon';
 import { Text } from '@components/ui/text';
 import { Link, Stack } from 'expo-router';
-import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
-import { useColorScheme } from 'nativewind';
+import { StarIcon } from 'lucide-react-native';
 import * as React from 'react';
-import { Image, type ImageStyle, View } from 'react-native';
-
-const LOGO = {
-	dark: require('../assets/images/react-native-reusables-dark.png'),
-	light: require('../assets/images/react-native-reusables-light.png'),
-};
+import { View } from 'react-native';
 
 const SCREEN_OPTIONS = {
-	headerRight: () => <ThemeToggle />,
 	headerTransparent: true,
 	title: 'React Native Reusables',
 };
 
-const IMAGE_STYLE: ImageStyle = {
-	height: 76,
-	width: 76,
-};
-
 export default function Screen() {
-	const { colorScheme } = useColorScheme();
-
 	return (
 		<>
 			<Stack.Screen options={SCREEN_OPTIONS} />
 			<View className="flex-1 items-center justify-center gap-8 p-4">
-				<Image resizeMode="contain" source={LOGO[colorScheme ?? 'light']} style={IMAGE_STYLE} />
 				<View className="gap-2 p-4">
 					<Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
 						1. Edit <Text variant="code">app/index.tsx</Text> to get started.
@@ -54,24 +39,5 @@ export default function Screen() {
 				</View>
 			</View>
 		</>
-	);
-}
-
-const THEME_ICONS = {
-	dark: MoonStarIcon,
-	light: SunIcon,
-};
-
-function ThemeToggle() {
-	const { colorScheme, toggleColorScheme } = useColorScheme();
-
-	return (
-		<Button
-			className="ios:size-9 rounded-full web:mx-4"
-			onPressIn={toggleColorScheme}
-			size="icon"
-			variant="ghost">
-			<Icon as={THEME_ICONS[colorScheme ?? 'light']} className="size-5" />
-		</Button>
 	);
 }
