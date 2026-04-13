4de0513467e202aa530e35b3ffeb002bde99b13d
Grishchenko, Ivan <bben.rasha@gmail.com>
2026-04-13 18:45:16 +0300

    chore(): setup react compiler

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import expoConfig from 'eslint-config-expo/flat.js';
 import perfectionist from 'eslint-plugin-perfectionist';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import reactCompiler from 'eslint-plugin-react-compiler';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
 	expoConfig,
 	eslintPluginPrettier,
+	reactCompiler.configs.recommended,
 	perfectionist.configs['recommended-natural'],
 	{ ignores: ['dist/*'] },
 ]);
